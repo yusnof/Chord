2d51be6a4ca26f5d29f0958e96728d8ab1a78aab
yusnof <yusnofal@gmail.com>
2025-12-05 11:10:42 +0100

    broke main into 2 files to easier understand the logic

---
+++ inputHelpers.go
@@ -0,0 +1,98 @@
+package main
+import (
+    "flag"
+    "log"
+    "net"
+)
+type Config struct {
+    IPAddr   string
+    Port     int
+    JoinAddr string
+    JoinPort int
+    TS       int
+    TFF      int
+    TCP      int
+    R        int
+    I       string
+    Flag_first_node bool
+}
+
+func Loadconfig() Config {
+	cfg := Config{}
+
+	flag.StringVar(&cfg.IPAddr, "a", "127.0.0.1", "Chord IP Address")
+	flag.IntVar(&cfg.Port, "p", 1234, "The port that the Chord client will bind to and listen on. Represented as a base-10 integer.")
+
+	flag.StringVar(&cfg.JoinAddr, "ja", "", "The IP address of the machine running a Chord node. The Chord client will join this node’s ring")
+
+	flag.IntVar(&cfg.JoinPort, "jp", 0, "The port that an existing Chord node is bound to and listening on.")
+	flag.IntVar(&cfg.TS, "ts", 3000, "The time in milliseconds between invocations of ‘stabilize’. Represented as a base-10 integer.")
+	flag.IntVar(&cfg.TFF, "tff", 1000, "The time in milliseconds between invocations of ‘fix fingers’.")
+	flag.IntVar(&cfg.TCP, "tcp", 3000, "The time in milliseconds between invocations of check predecessor’")
+	flag.IntVar(&cfg.R, "r", 4, "The number of successors maintained by the Chord client. ")
+	flag.StringVar(&cfg.I, "i", "", "he identifier (ID) assigned to the Chord client which will override the ID computed by the SHA1 sum of the client’s IP address and port number.")
+
+	flag.Parse()
+
+	validateInput(cfg)
+
+	return cfg
+
+}
+func validateInput(cfg Config) {
+	cfg.Flag_first_node = true
+
+	if cfg.IPAddr == "" || net.ParseIP(cfg.IPAddr).To4() == nil {
+		log.Fatal("-a must be a valid IPv4 address, e.g. 128.8.126.63")
+	}
+	if cfg.Port < 1024 || cfg.Port > 65535 {
+		log.Fatal("-p must be in range 1024–65535")
+	}
+
+	if (cfg.JoinAddr == "" && cfg.JoinPort != 0) || (cfg.JoinAddr != "" && cfg.JoinPort == 0) {
+		log.Fatal("--ja and --jp must be given together, or neither")
+	}
+
+	if cfg.JoinAddr != "" && cfg.JoinPort != 0 {
+		cfg.Flag_first_node= false
+		if net.ParseIP(cfg.JoinAddr).To4() == nil {
+			log.Fatal("--ja must be a valid IPv4 address")
+		}
+		if cfg.JoinPort < 1024 || cfg.JoinPort > 65535 {
+			log.Fatal("--jp must be in range 1024–65535")
+		}
+	}
+
+	if cfg.TS <= 1 || cfg.TS >= 60000 {
+		log.Fatal("Ts must be specified, or between 1 and 60k")
+
+	}
+	if cfg.TFF <= 1 || cfg.TFF >= 60000 {
+		log.Fatal("Tff must be specified, or between 1 and 60k")
+
+	}
+	if cfg.TCP <= 1 || cfg.TCP >= 60000 {
+		log.Fatal("tcp must be specified, or between 1 and 60k")
+
+	}
+	if cfg.R <= 1 || cfg.R >= 32 {
+		log.Fatal("R must be specified, or between 1 and 32")
+	}
+	if cfg.I != "" {
+		if len(cfg.I) != 40 || !isHex(cfg.I) {
+			log.Fatal("-i must be 40 hex chars (0-9a-fA-F)")
+		}
+
+	}
+}
+
+func isHex(s string) bool {
+	for _, c := range s {
+		if !((c >= '0' && c <= '9') ||
+			(c >= 'a' && c <= 'f') ||
+			(c >= 'A' && c <= 'F')) {
+			return false
+		}
+	}
+	return true
+}
--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"bufio"
 	pb "chord/protocol"
-	"io"
-	"os"
-
-	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"os"
 	"strings"
 	"time"
 
@@ -17,36 +15,12 @@ import (
 )
 
 var (
-	flag_first_node bool
-	localaddress    string // TODO to be removed
+	localaddress string // TODO to be removed
 )
-
-func loadconfig() Config {
-	cfg := Config{}
-
-	flag.StringVar(&cfg.IPAddr, "a", "127.0.0.1", "Chord IP Address")
-	flag.IntVar(&cfg.Port, "p", 1234, "The port that the Chord client will bind to and listen on. Represented as a base-10 integer.")
-
-	flag.StringVar(&cfg.JoinAddr, "ja", "", "The IP address of the machine running a Chord node. The Chord client will join this node’s ring")
-
-	flag.IntVar(&cfg.JoinPort, "jp", 0, "The port that an existing Chord node is bound to and listening on.")
-	flag.IntVar(&cfg.TS, "ts", 3000, "The time in milliseconds between invocations of ‘stabilize’. Represented as a base-10 integer.")
-	flag.IntVar(&cfg.TFF, "tff", 1000, "The time in milliseconds between invocations of ‘fix fingers’.")
-	flag.IntVar(&cfg.TCP, "tcp", 3000, "The time in milliseconds between invocations of check predecessor’")
-	flag.IntVar(&cfg.R, "r", 4, "The number of successors maintained by the Chord client. ")
-	flag.StringVar(&cfg.I, "i", "", "he identifier (ID) assigned to the Chord client which will override the ID computed by the SHA1 sum of the client’s IP address and port number.")
-
-	flag.Parse()
-
-	validateInput(cfg)
-
-	return cfg
-
-}
 
 func main() {
 
-	cfg := loadconfig()
+	cfg := Loadconfig()
 
 	node := StartServer(cfg)
 
@@ -145,7 +119,7 @@ func StartServer(cfg Config) *Node {
 			log.Fatalf("failed to serve: %v", err)
 		}
 	}()
-	if flag_first_node {
+	if cfg.Flag_first_node {
 		node.Successors = []string{node.Address}
 	} else {
 		// node.Successors = []string{nprime} TODO not sure about this one
@@ -166,64 +140,6 @@ func StartServer(cfg Config) *Node {
 	}()
 
 	return node
-}
-
-func validateInput(cfg Config) {
-	flag_first_node = true
-
-	if cfg.IPAddr == "" || net.ParseIP(cfg.IPAddr).To4() == nil {
-		log.Fatal("-a must be a valid IPv4 address, e.g. 128.8.126.63")
-	}
-	if cfg.Port < 1024 || cfg.Port > 65535 {
-		log.Fatal("-p must be in range 1024–65535")
-	}
-
-	if (cfg.JoinAddr == "" && cfg.JoinPort != 0) || (cfg.JoinAddr != "" && cfg.JoinPort == 0) {
-		log.Fatal("--ja and --jp must be given together, or neither")
-	}
-
-	if cfg.JoinAddr != "" && cfg.JoinPort != 0 {
-		flag_first_node = false
-		if net.ParseIP(cfg.JoinAddr).To4() == nil {
-			log.Fatal("--ja must be a valid IPv4 address")
-		}
-		if cfg.JoinPort < 1024 || cfg.JoinPort > 65535 {
-			log.Fatal("--jp must be in range 1024–65535")
-		}
-	}
-
-	if cfg.TS <= 1 || cfg.TS >= 60000 {
-		log.Fatal("Ts must be specified, or between 1 and 60k")
-
-	}
-	if cfg.TFF <= 1 || cfg.TFF >= 60000 {
-		log.Fatal("Tff must be specified, or between 1 and 60k")
-
-	}
-	if cfg.TCP <= 1 || cfg.TCP >= 60000 {
-		log.Fatal("tcp must be specified, or between 1 and 60k")
-
-	}
-	if cfg.R <= 1 || cfg.R >= 32 {
-		log.Fatal("R must be specified, or between 1 and 32")
-	}
-	if cfg.I != "" {
-		if len(cfg.I) != 40 || !isHex(cfg.I) {
-			log.Fatal("-i must be 40 hex chars (0-9a-fA-F)")
-		}
-
-	}
-}
-
-func isHex(s string) bool {
-	for _, c := range s {
-		if !((c >= '0' && c <= '9') ||
-			(c >= 'a' && c <= 'f') ||
-			(c >= 'A' && c <= 'F')) {
-			return false
-		}
-	}
-	return true
 }
 
 func resolveAddress(address string) string {
